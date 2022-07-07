0f832d21e02906500574b4f0b5b61c479693442f
Your Name <you@example.com>
2022-07-07 14:10:56 +0700

    0.1 First Version only with start logo

---
+++ app/src/main/java/com/ivan/hvhhelper/StartLogoActivity.java
@@ -0,0 +1,32 @@
+package com.ivan.hvhhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class StartLogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_logo);
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+        new CountDownTimer(1900,1000){
+            @Override
+            public void onTick(long millisUntilFinished){}
+
+            @Override
+            public void onFinish(){
+                startActivity(intent);
+            }
+        }.start();
+
+
+
+
+    }
+}
